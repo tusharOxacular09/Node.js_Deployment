38f3504a93c373da823378b5cb2e189ec83d9097
Tushar Kanta Swain <tushar.swain@credencesoft.in>
2024-04-13 10:30:49 +0530

    Modifying the root endpoint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import "dotenv/config.js"
 
 const app = express();
 
+// http://54.91.164.203:7002/health-check
+app.get("/", (req, res) => {
+  return res.status(200).json({
+      success: true,
+      message: "Welcome to Tushar's ultimate DEVOPS Journey, Hope Maa Kali will be happy with her children's progress 😊."
+  });
+});
+
 app.get("/health-check", (req, res) => {
   return res.status(200).json({
     success: true,
